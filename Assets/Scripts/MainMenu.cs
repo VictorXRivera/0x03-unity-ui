d34f56e6997725d242f8c22cdd31609cc2cbff3f
VictorXRivera <victorxrivera.professional@gmail.com>
2021-01-07 14:00:20 -0500

    MainMenu script and also created Play,Options,Quit and Back button

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayMaze()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+    }
+}
